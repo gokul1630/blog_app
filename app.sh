d185ee3e427020171a0801ec7854c66d7a369956
gokul1630 <gokulgp3167@gmail.com>
2024-12-19 16:53:54 +0530

    setup frontend on non-sudoer user

--- app.sh
+++ app.sh
@@ -2,29 +2,29 @@
 
 sudo yum update -y
 sudo yum upgrade -y
-sudo yum install nginx git ncurses wget -y
+sudo yum install nginx git ncurses wget lsof -y
 
-cd /tmp
+curl -o- https://raw.githubusercontent.com/gokul1630/blog_app_Iac/refs/heads/main/nginx.conf > nginx.conf
+
+sudo mv nginx.conf /etc/nginx/nginx.conf
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+sudo useradd frontend && sudo su frontend
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 source ~/.bashrc
 
-npm install pm2 -g
+nvm install 20 && nvm use 20
 
 git clone https://github.com/gokul1630/blog_app_Iac blog_app
 
-cd blog_app && ls
+cd blog_app/frontend
 
-sudo mv nginx.conf /etc/nginx/nginx.conf
-
-cd frontend && ls
+npm install pm2 -g
 
 npm install --legacy-peer-deps && npm run build 
 
 pm2 start npm --name devops-blog-app -- run start -- -p 3000
-
-
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo rm -rf /tmp/blog_app
